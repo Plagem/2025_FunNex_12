494d4024ee431fb190dd0d38a56228d79603eaae
parang56 <parang56@soongsil.ac.kr>
2025-07-19 20:05:20 +0900

    Main Menu V0.1
    - 캐릭터 쏴서 시작 버튼, 종료 버튼과 부딪히면 그에 맞게 작동

---
+++ Assets/Scripts/UI/CursorManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [Header("Cursor Textures")]
+    public Texture2D defaultCursor;
+    public Texture2D clickCursor;
+
+    [Header("Hotspot")]
+    public Vector2 hotspot = Vector2.zero;
+
+    private void Start()
+    {
+        SetDefaultCursor();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetClickCursor();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            SetDefaultCursor();
+        }
+    }
+
+    public void SetDefaultCursor()
+    {
+        Cursor.SetCursor(defaultCursor, hotspot, CursorMode.Auto);
+    }
+
+    public void SetClickCursor()
+    {
+        Cursor.SetCursor(clickCursor, hotspot, CursorMode.Auto);
+    }
+}
