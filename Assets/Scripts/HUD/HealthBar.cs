ea5ed9462951dbe9bfb80d0fd222e928da5b3f87
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-07-19 11:38:27 +0900

    feat : HpBar 추가

--- Assets/Scripts/HUD/HealthBar.cs
+++ Assets/Scripts/HUD/HealthBar.cs
@@ -1,16 +1,27 @@
 using System;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class HealthBar : MonoBehaviour
 {
-    private BaseStatComponent _statComponent;
-    private ProgressBar _healthBar;
+    [SerializeField] private BaseStatComponent _statComponent;
+    
+    [SerializeField] private TMP_Text _healthText;
+    [SerializeField] private Slider _healthBar;
+    
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 1f, 0);
     
     private void Start()
     {
-        _statComponent = GetComponent<BaseStatComponent>();
         _statComponent.OnAttributeChanged += OnAttributeChanged;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+        transform.rotation = Quaternion.identity;
     }
 
     private void OnAttributeChanged(StatType statType, AttributeData attributeData)
@@ -18,8 +29,11 @@ public class HealthBar : MonoBehaviour
         if (statType == StatType.MaxHealth || statType == StatType.CurrentHealth)
         {
             float HealthPercent = _statComponent.GetCurrentValue(StatType.CurrentHealth) /
-                                  _statComponent.GetCurrentValue(StatType.MaxHealth) * 100;
-            Debug.Log($"Health Percent : {HealthPercent}%");
+                                  _statComponent.GetCurrentValue(StatType.MaxHealth);
+            _healthText.SetText($"{_statComponent.GetCurrentValue(StatType.CurrentHealth)}");
+            _healthBar.value = HealthPercent;
+            
+            Debug.Log("DLKHJFIOEU");
         }
     }
 }
